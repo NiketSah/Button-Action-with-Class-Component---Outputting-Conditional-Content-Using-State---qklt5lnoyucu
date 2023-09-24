02387c48b5be6c49f716d0f34310c3370294cb94
NiketSah <117289451+NiketSah@users.noreply.github.com>
2023-09-24 23:22:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,29 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+    this.state = {
+      isButtonClicked: false, // Initialize the state to false
 	};
+    }
+	handleButtonClick = () => {
+    this.setState({ isButtonClicked: true });
+  };
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+		 { /* Render the button with an onClick event handler */ }
+        <button id="click" onClick={this.handleButtonClick}>
+          Click me
+        </button>
+
+        { /* Conditionally render the paragraph based on the state */ }
+        {this.state.isButtonClicked && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+          </p>
+        )}
     		</div>
     	);
     }
