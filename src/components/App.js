a9eb8dd9bf1ce7849bd564cb13a8935334e5a4c0
NiketSah <117289451+NiketSah@users.noreply.github.com>
2023-09-24 23:28:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component } from 'react';
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
     this.state = {
       isButtonClicked: false, // Initialize the state to false
-	};
-    }
-	handleButtonClick = () => {
+    };
+  }
+
+  // Function to handle button click and update the state
+  handleButtonClick = () => {
     this.setState({ isButtonClicked: true });
   };
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-		 { /* Render the button with an onClick event handler */ }
+  render() {
+    return (
+      <div id="main">
+        { /* Render the button with an onClick event handler */ }
         <button id="click" onClick={this.handleButtonClick}>
           Click me
         </button>
@@ -27,11 +28,11 @@ class App extends Component {
             Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
           </p>
         )}
-    		</div>
-    	);
-    }
+      </div>
+    );
+  }
 }
 
-
 export default App;
+
 
